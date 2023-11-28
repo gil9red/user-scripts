61a1d1ab0f555db5aed92594893f8c3922bd184c
Ilya Petrash <ilya.petrash@inbox.ru>
2023-11-28 21:46:41 +0500

    Create main.user.js

---
+++ youtube/Спрятать вкладки/main.user.js
@@ -0,0 +1,38 @@
+// ==UserScript==
+// @name         Youtube. Спрятать вкладки "Сообщество" и "Каналы"
+// @namespace    http://tampermonkey.net/
+// @version      0.4
+// @description  try to take over the world!
+// @author       gil9red
+// @match        https://www.youtube.com/@*
+// @match        https://www.youtube.com/channel/*
+// @require      https://gist.githubusercontent.com/mjblay/18d34d861e981b7785e407c3b443b99b/raw/debc0e6d4d537ac228d1d71f44b1162979a5278c/waitForKeyElements.js
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=youtube.com
+// @grant        none
+// @homepage     https://gist.github.com/gil9red/f563e7b2fa304df1009d33d0fa361ee9
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    function hideTabs(titles) {
+        function processText(text) {
+            return text == null ? null : text.trim().toLowerCase();
+        }
+
+        titles = titles.map(processText);
+
+        waitForKeyElements(
+            "#tabs-container yt-tab-shape",
+            (tabEl) => {
+                let tabText = tabEl.getAttribute("tab-title");
+                tabText = processText(tabText);
+                if (titles.includes(tabText)) {
+                    tabEl.style.display = 'none';
+                }
+            }
+        );
+    }
+
+    hideTabs(["Сообщество", "Каналы"]);
+})();
