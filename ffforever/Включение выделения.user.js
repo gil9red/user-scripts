e5ad8b018d3a6e4c3c77c2f7f3fb00e7ca8890e7
Ilya Petrash <ilya.petrash@inbox.ru>
2023-11-29 21:22:38 +0500

    Рефакторинг

--- ffforever/Включение выделения.user.js
+++ ffforever/Включение выделения.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Включение выделения
 // @namespace    gil9red
-// @version      0.1
+// @version      0.2
 // @description  Отключение выключения выделения текста
 // @author       gil9red
 // @match        https://ffforever.info/index.cgi*
@@ -15,17 +15,13 @@
 (function() {
     'use strict';
 
-    // NOTE: Нужно удалить:
-    //    Из <html>:
-    //        Класс "no-touch"
-    //
-    //    Из одного единственного <td>:
-    //        Значения "*-user-select: none;" из style="padding-top:10px;padding-bottom:10px;padding-left:25px;padding-right:25px;line-height:15px;font-size:12px;-moz-user-select: none;-khtml-user-select: none;"
-    //        Атрибут: onselectstart="return false"
-
+    // Удаление атрибута функции onselectstart
+    //     Пример: onselectstart="return false"
     let el_onselectstart = document.querySelector("td[onselectstart]");
     el_onselectstart.removeAttribute("onselectstart");
 
+    // Удаление стилей:
+    //     Пример: "*-user-select: none;" из style="padding-top:10px;padding-bottom:10px;padding-left:25px;padding-right:25px;line-height:15px;font-size:12px;-moz-user-select: none;-khtml-user-select: none;"
     for (let style_prop of ['-moz-user-select', '-khtml-user-select']) {
         if (el_onselectstart.style.removeProperty) {
             el_onselectstart.style.removeProperty(style_prop);
@@ -36,11 +32,11 @@
         }
     }
 
+    // Удаление класса "no-touch"
     // Класс не сразу добавляется сайтом, поэтому нужно будет повторить проверку
     var intervalId = setInterval(() => {
-        let el_not_touch = document.querySelector(".no-touch");
-        if (el_not_touch) {
-            el_not_touch.classList.remove("no-touch");
+        for (let el of document.querySelectorAll(".no-touch")) {
+            el.classList.remove("no-touch");
             clearInterval(intervalId);
         }
     }, 1000);
