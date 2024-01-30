17ae7a40787bda0dea70ff97e7bde56b9864d060
Ilya Petrash <ilya.petrash@inbox.ru>
2024-01-30 16:18:04 +0500

    Поддержка заголовка и для не закрепленных заметок

--- google/Google Keep. Заголовок вкладки из заголовка заметок.user.js
+++ google/Google Keep. Заголовок вкладки из заголовка заметок.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Google Keep. Заголовок вкладки из заголовка заметок
 // @namespace    gil9red
-// @version      0.3
+// @version      0.4
 // @description  try to take over the world!
 // @author       You
 // @match        https://keep.google.com/*
@@ -15,35 +15,50 @@
 (function() {
     'use strict';
 
-    // Например: "#LIST/" и "#NOTE/"
-    const serverId = location.href.split(/#\w+\//)[1];
-    if (!serverId) {
-        return;
-    }
+    function process() {
+        let foundTitle = false;
 
-    let chunkJsonText = null;
+        // Например: "#LIST/" и "#NOTE/"
+        const serverId = location.href.split(/#\w+\//)[1];
+        if (serverId) {
+            let chunkJsonText = null;
 
-    let scripts = document.querySelectorAll("script");
-    for (let i = 0; i < scripts.length; i++) {
-        let scriptText = scripts[i].innerText;
-        let m = scriptText.match(/loadChunk\(JSON\.parse\(['"](.+?)['"]\)/);
-        if (m) {
-            // Символы экранированы в ASCII
-            chunkJsonText = eval('"' + m[1] + '"');
-            break;
+            // Работает для закрепленных
+            let scripts = document.querySelectorAll("script");
+            for (let i = 0; i < scripts.length; i++) {
+                let scriptText = scripts[i].innerText;
+                let m = scriptText.match(/loadChunk\(JSON\.parse\(['"](.+?)['"]\)/);
+                if (m) {
+                    // Символы экранированы в ASCII
+                    chunkJsonText = eval('"' + m[1] + '"');
+                    break;
+                }
+            }
+
+            if (!chunkJsonText) {
+                alert("Не удалось найти JSON из loadChunk!");
+                return;
+            }
+
+            let chunkData = JSON.parse(chunkJsonText);
+            for (let data of chunkData) {
+                if (data.serverId == serverId) {
+                    document.title = data.title;
+                    foundTitle = true;
+                    break;
+                }
+            }
+        }
+
+        if (!foundTitle) {
+            // Поиск от кнопки в заметке и к определенному элементу по порядку на том же уровне вложенности
+            const cssSelector = '[data-tooltip-text="Закрепить заметку"] ~ div:nth-child(4) > div[contenteditable="true"][role="textbox"][dir="ltr"]';
+
+            let itemEl = document.querySelector(cssSelector);
+            if (itemEl && itemEl.innerText) {
+                document.title = itemEl.innerText;
+            }
         }
     }
-
-    if (!chunkJsonText) {
-        alert("Не удалось найти JSON из loadChunk!");
-        return;
-    }
-
-    let chunkData = JSON.parse(chunkJsonText);
-    for (let data of chunkData) {
-        if (data.serverId == serverId) {
-            document.title = data.title;
-            break;
-        }
-    }
+    setInterval(process, 1000);
 })();
