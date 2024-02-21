2af08f2e4500454dba0c8ecaf906b08ce98aefcc
Ilya Petrash <ilya.petrash@inbox.ru>
2024-02-21 12:11:42 +0500

    Обновление. Улучшение алгоритма поиска заголовка заметки

--- google/Google Keep. Заголовок вкладки из заголовка заметок.user.js
+++ google/Google Keep. Заголовок вкладки из заголовка заметок.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Google Keep. Заголовок вкладки из заголовка заметок
 // @namespace    gil9red
-// @version      0.4
+// @version      0.5
 // @description  try to take over the world!
 // @author       You
 // @match        https://keep.google.com/*
@@ -16,49 +16,15 @@
     'use strict';
 
     function process() {
-        let foundTitle = false;
-
         // Например: "#LIST/" и "#NOTE/"
-        const serverId = location.href.split(/#\w+\//)[1];
+        const serverId = location.href.split(/#\w+\//)[1]; // Так понимаем, что это выбранная заметка
         if (serverId) {
-            let chunkJsonText = null;
-
-            // Работает для закрепленных
-            let scripts = document.querySelectorAll("script");
-            for (let i = 0; i < scripts.length; i++) {
-                let scriptText = scripts[i].innerText;
-                let m = scriptText.match(/loadChunk\(JSON\.parse\(['"](.+?)['"]\)/);
-                if (m) {
-                    // Символы экранированы в ASCII
-                    chunkJsonText = eval('"' + m[1] + '"');
-                    break;
-                }
-            }
-
-            if (!chunkJsonText) {
-                alert("Не удалось найти JSON из loadChunk!");
-                return;
-            }
-
-            let chunkData = JSON.parse(chunkJsonText);
-            for (let data of chunkData) {
-                if (data.serverId == serverId) {
-                    document.title = data.title;
-                    foundTitle = true;
-                    break;
-                }
-            }
-        }
-
-        if (!foundTitle) {
-            // Поиск от кнопки в заметке и к определенному элементу по порядку на том же уровне вложенности
-            const cssSelector = '[data-tooltip-text="Закрепить заметку"] ~ div:nth-child(4) > div[contenteditable="true"][role="textbox"][dir="ltr"]';
-
-            let itemEl = document.querySelector(cssSelector);
+            let itemEl = document.querySelector('#keep-iape');
             if (itemEl && itemEl.innerText) {
                 document.title = itemEl.innerText;
             }
         }
     }
+
     setInterval(process, 1000);
 })();
