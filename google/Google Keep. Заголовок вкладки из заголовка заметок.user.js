ff7bf39c779968f9b59dfe1b8f5dcb5cac6ef1e0
Ilya Petrash <ilya.petrash@inbox.ru>
2024-02-26 12:41:46 +0500

    Исправление ошибки в поиске заголовка заметки

--- google/Google Keep. Заголовок вкладки из заголовка заметок.user.js
+++ google/Google Keep. Заголовок вкладки из заголовка заметок.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Google Keep. Заголовок вкладки из заголовка заметок
 // @namespace    gil9red
-// @version      0.5
+// @version      0.6
 // @description  try to take over the world!
 // @author       You
 // @match        https://keep.google.com/*
@@ -15,15 +15,55 @@
 (function() {
     'use strict';
 
+    let oldDocumentTitle = document.title;
+
     function process() {
         // Например: "#LIST/" и "#NOTE/"
         const serverId = location.href.split(/#\w+\//)[1]; // Так понимаем, что это выбранная заметка
         if (serverId) {
             let itemEl = document.querySelector('#keep-iape');
+            if (itemEl && itemEl.innerText != "Новая заметка") {
+                document.title = itemEl.innerText;
+                return;
+            }
+
+            let chunkJsonText = null;
+
+            // Работает для закрепленных
+            let scripts = document.querySelectorAll("script");
+            for (let i = 0; i < scripts.length; i++) {
+                let scriptText = scripts[i].innerText;
+                let m = scriptText.match(/loadChunk\(JSON\.parse\(['"](.+?)['"]\)/);
+                if (m) {
+                    // Символы экранированы в ASCII
+                    chunkJsonText = eval('"' + m[1] + '"');
+                    break;
+                }
+            }
+
+            if (!chunkJsonText) {
+                alert("Не удалось найти JSON из loadChunk!");
+                return;
+            }
+
+            let chunkData = JSON.parse(chunkJsonText);
+            for (let data of chunkData) {
+                if (data.serverId == serverId) {
+                    document.title = data.title;
+                    return;
+                }
+            }
+
+            // Поиск от кнопки в заметке и к определенному элементу по порядку на том же уровне вложенности
+            const cssSelector = '[data-tooltip-text="Закрепить заметку"] ~ div:nth-child(4) > div[contenteditable="true"][role="textbox"][dir="ltr"]';
+
+            itemEl = document.querySelector(cssSelector);
             if (itemEl && itemEl.innerText) {
                 document.title = itemEl.innerText;
             }
         }
+
+        document.title = oldDocumentTitle;
     }
 
     setInterval(process, 1000);
