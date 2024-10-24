6ce82e9bd97214f5c931cd2ff9232fffbcb221d0
Ilya Petrash <ilya.petrash@inbox.ru>
2024-10-24 15:28:19 +0500

    Jira. Кнопка копирования номера задачи в буфер обмена

---
+++ helpdesk.compassluxe/Кнопка копирования номера задачи в буфер обмена.user.js
@@ -0,0 +1,46 @@
+// ==UserScript==
+// @name         Jira. Кнопка копирования номера задачи в буфер обмена
+// @namespace    gil9red
+// @version      2024-10-24
+// @description  try to take over the world!
+// @author       gil9red
+// @match        https://helpdesk.compassluxe.com/browse/*
+// @icon         https://helpdesk.compassluxe.com/s/5krrdz/712004/7fcd86bd1fac8f876f6db741a6132e00/_/images/fav-jsw.png
+// @grant        GM_setClipboard
+// @homepage     https://github.com/gil9red/user-scripts
+// @updateURL    https://github.com/gil9red/user-scripts/raw/main/helpdesk.compassluxe/Кнопка копирования номера задачи в буфер обмена.user.js
+// @downloadURL  https://github.com/gil9red/user-scripts/raw/main/helpdesk.compassluxe/Кнопка копирования номера задачи в буфер обмена.user.js
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    const id = "coping-jira-key-to-clipboard";
+    $(".aui-header-primary > .aui-nav").append(
+        `
+        <li>
+            <span
+                id="${id}"
+                class="aui-button aui-button-primary aui-style"
+                title="Coping jira key to clipboard"
+            >
+                📋<span class="info"></span>
+            </span>
+        </li>
+        `
+    );
+    $(`#${id}`).click(function() {
+        let jiraId = $(`meta[name="ajs-issue-key"]`).attr("content");
+
+        let $info = $(this).find(".info");
+        $info.text(" - coping...");
+        GM_setClipboard(
+            jiraId,
+            "text",
+            () => {
+                $info.text(" - coping finished!");
+                setTimeout(() => $info.text(""), 1500);
+            }
+        );
+    });
+})();
