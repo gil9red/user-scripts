64265a489b309033e150d0ee3e75bbc864bb3698
Ilya Petrash <ilya.petrash@inbox.ru>
2025-12-08 00:57:30 +0500

    Create Установка громкости при открытии вкладки.user.js

---
+++ rutube/Установка громкости при открытии вкладки.user.js
@@ -0,0 +1,40 @@
+// ==UserScript==
+// @name         Rutube. Установка громкости при открытии вкладки
+// @namespace    gil9red
+// @version      2025-12-08
+// @description  try to take over the world!
+// @author       gil9red
+// @match        https://rutube.ru/video/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=rutube.ru
+// @updateURL    https://github.com/gil9red/user-scripts/raw/main/rutube/Установка%20громкости%20при%20открытии%20вкладки.user.js
+// @downloadURL  https://github.com/gil9red/user-scripts/raw/main/rutube/Установка%20громкости%20при%20открытии%20вкладки.user.js
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    var videoByProcessed = new Map();
+
+    function process() {
+        for (let video of document.getElementsByTagName("video")) {
+            if (!videoByProcessed.has(video)) {
+                videoByProcessed.set(video, false);
+            }
+
+            if (videoByProcessed.get(video)) {
+                continue;
+            }
+            
+            video.volume = 0.15;
+            videoByProcessed.set(video, true);
+        }
+    }
+
+    process();
+
+    var intervalId = setInterval(process, 100);
+
+    // Думаю, 2 секунд хватит, чтобы обработать видео
+    setTimeout(() => clearInterval(intervalId), 2000);
+})();
