45a2d4e968037fa05513537528cd6e3f7cdfbe82
Ilya Petrash <ilya.petrash@inbox.ru>
2026-01-02 04:29:09 +0500

    Update and rename MultFan. Настройка громности и скорости.user.js to MultFan. Настройка громкости и скорости.user.js

--- MultFan. Настройка громкости и скорости.user.js
+++ MultFan. Настройка громкости и скорости.user.js
@@ -1,13 +1,13 @@
 // ==UserScript==
-// @name         MultFan. Настройка громности и скорости
+// @name         MultFan. Настройка громкости и скорости
 // @namespace    gil9red
 // @version      2025-12-15
 // @description  try to take over the world!
 // @author       gil9red
 // @match        https://*.mult-fan.tv/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=mult-fan.tv
-// @updateURL    https://github.com/gil9red/user-scripts/raw/main/MultFan.%20Настройка%20громности%20и%20скорости.user.js
-// @downloadURL  https://github.com/gil9red/user-scripts/raw/main/MultFan.%20Настройка%20громности%20и%20скорости.user.js
+// @updateURL    https://github.com/gil9red/user-scripts/raw/main/MultFan.%20Настройка%20громкости%20и%20скорости.user.js
+// @downloadURL  https://github.com/gil9red/user-scripts/raw/main/MultFan.%20Настройка%20громкости%20и%20скорости.user.js
 // @grant        none
 // ==/UserScript==
 
