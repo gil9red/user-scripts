681c8d627524b218534f94ba24666b01b3488a1f
Ilya Petrash <ilya.petrash@inbox.ru>
2026-06-06 10:59:51 +0500

    Update Early access indicator in tab title.user.js

--- steam/Early access indicator in tab title.user.js
+++ steam/Early access indicator in tab title.user.js
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @name         Steam. Early access indicator in tab title
 // @namespace    gil9red
-// @version      2025-09-11
+// @version      2026-06-06
 // @description  try to take over the world!
 // @author       gil9red
 // @match        https://store.steampowered.com/app/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=steampowered.com
-// @homepage     https://github.com/gil9red/user-scripts
+// @homepage     https://github.com/gil9red/user-scripts/blob/main/steam/Early%20access%20indicator%20in%20tab%20title.user.js
 // @updateURL    https://github.com/gil9red/user-scripts/raw/main/steam/Early%20access%20indicator%20in%20tab%20title.user.js
 // @downloadURL  https://github.com/gil9red/user-scripts/raw/main/steam/Early%20access%20indicator%20in%20tab%20title.user.js
 // @grant        none
@@ -15,8 +15,8 @@
 (function() {
     'use strict';
 
-    let earlyAccessHeaderEl = document.getElementById("earlyAccessHeader");
-    if (earlyAccessHeaderEl) {
-        document.title = `🚧 ${document.title}`;
+    const earlyAccessHeader = document.getElementById("earlyAccessHeader");
+    if (earlyAccessHeader) {
+        document.title = `🐣${document.title}`;
     }
 })();
