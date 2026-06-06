562df7deeed951abb454d357ce6fc203be0144c2
Ilya Petrash <ilya.petrash@inbox.ru>
2026-06-06 11:02:19 +0500

    Update Coming soon indicator in tab title.user.js

--- steam/Coming soon indicator in tab title.user.js
+++ steam/Coming soon indicator in tab title.user.js
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @name         Steam. Coming soon indicator in tab title
 // @namespace    gil9red
-// @version      2025-09-11
+// @version      2026-06-06
 // @description  try to take over the world!
 // @author       gil9red
 // @match        https://store.steampowered.com/app/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=steampowered.com
-// @homepage     https://github.com/gil9red/user-scripts
+// @homepage     https://github.com/gil9red/user-scripts/blob/main/steam/Coming%20soon%20indicator%20in%20tab%20title.user.js
 // @updateURL    https://github.com/gil9red/user-scripts/raw/main/steam/Coming%20soon%20indicator%20in%20tab%20title.user.js
 // @downloadURL  https://github.com/gil9red/user-scripts/raw/main/steam/Coming%20soon%20indicator%20in%20tab%20title.user.js
 // @grant        none
@@ -15,7 +15,8 @@
 (function() {
     'use strict';
 
-    if (document.querySelector(".game_area_comingsoon")) {
-        document.title = `⛔ ${document.title}`;
+    const comingSoon = document.querySelector(".game_area_comingsoon");
+    if (comingSoon) {
+        document.title = `⏳${document.title}`;
     }
 })();
