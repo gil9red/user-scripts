5b81d245ebf87ad05864cd6a0af35f64b3821a0d
Ilya Petrash <ilya.petrash@inbox.ru>
2026-06-24 13:29:06 +0500

    Update Добавление кнопок профилей у Assignee и Reporter.user.js

--- helpdesk.compassluxe/Добавление кнопок профилей у Assignee и Reporter.user.js
+++ helpdesk.compassluxe/Добавление кнопок профилей у Assignee и Reporter.user.js
@@ -1,14 +1,14 @@
 // ==UserScript==
 // @name         Jira. Добавление кнопок профилей у Assignee и Reporter
 // @namespace    gil9red
-// @version      2024-11-16
+// @version      2026-06-24
 // @description  try to take over the world!
 // @author       gil9red
 // @match        https://helpdesk.compassluxe.com/browse/*
 // @match        https://helpdesk.compassluxe.com/projects/*/issues/*
 // @icon         https://helpdesk.compassluxe.com/s/5krrdz/712004/7fcd86bd1fac8f876f6db741a6132e00/_/images/fav-jsw.png
 // @grant        none
-// @homepage     https://github.com/gil9red/user-scripts
+// @homepage     https://github.com/gil9red/user-scripts/blob/main/helpdesk.compassluxe/Добавление кнопок профилей у Assignee и Reporter.user.js
 // @updateURL    https://github.com/gil9red/user-scripts/raw/main/helpdesk.compassluxe/Добавление кнопок профилей у Assignee и Reporter.user.js
 // @downloadURL  https://github.com/gil9red/user-scripts/raw/main/helpdesk.compassluxe/Добавление кнопок профилей у Assignee и Reporter.user.js
 // ==/UserScript==
@@ -16,16 +16,34 @@
 (function() {
     'use strict';
 
-    let $assignee = $("#assignee-val .user-hover[rel]");
-    let $reporter = $("#reporter-val .user-hover[rel]");
-    if ($assignee.length == 0 || $reporter.length == 0) {
-        return;
-    }
+    const LINK_CLASS = 'tm-profile-link';
 
     function get_a(username) {
-        return `<a href="/secure/ViewProfile.jspa?name=${username}" target="_blank">➡️</a>`;
+        return `<a href="/secure/ViewProfile.jspa?name=${username}" target="_blank" class="${LINK_CLASS}">➡️</a>`;
     }
 
-    $assignee.append(get_a($assignee.attr("rel")));
-    $reporter.append(get_a($reporter.attr("rel")));
+    function processUserField(selector) {
+        const $el = document.querySelector(selector);
+        if (!$el) return;
+        if ($el.querySelector(`.${LINK_CLASS}`)) return;
+
+        const username = $el.getAttribute("rel");
+        if (!username) return;
+
+        $el.insertAdjacentHTML('beforeend', get_a(username));
+    }
+
+    function checkAndApply() {
+        processUserField("#assignee-val .user-hover[rel]");
+        processUserField("#reporter-val .user-hover[rel]");
+    }
+
+    const observer = new MutationObserver(() => {
+        checkAndApply();
+    });
+
+    observer.observe(document.documentElement, {
+        childList: true,
+        subtree: true
+    });
 })();
