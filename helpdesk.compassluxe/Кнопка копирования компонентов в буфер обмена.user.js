4de51862ef05e6cb678d2b833534987b70a2356f
Ilya Petrash <ilya.petrash@inbox.ru>
2026-07-15 13:37:28 +0500

    Update Кнопка копирования компонентов в буфер обмена.user.js

--- helpdesk.compassluxe/Кнопка копирования компонентов в буфер обмена.user.js
+++ helpdesk.compassluxe/Кнопка копирования компонентов в буфер обмена.user.js
@@ -1,33 +1,34 @@
 // ==UserScript==
 // @name         Jira. Кнопка копирования компонентов в буфер обмена
 // @namespace    gil9red
-// @version      2026-07-14
+// @version      2026-07-15
 // @description  try to take over the world!
 // @author       gil9red
 // @match        https://helpdesk.compassluxe.com/browse/*
 // @match        https://helpdesk.compassluxe.com/projects/*/issues/*
 // @icon         https://helpdesk.compassluxe.com/s/5krrdz/712004/7fcd86bd1fac8f876f6db741a6132e00/_/images/fav-jsw.png
 // @grant        GM_setClipboard
-// @homepage     https://github.com/gil9red/user-scripts/blob/main/helpdesk.compassluxe/Кнопка копирования компонентов в буфер обмена.user.js
-// @updateURL    https://github.com/gil9red/user-scripts/raw/main/helpdesk.compassluxe/Кнопка копирования компонентов в буфер обмена.user.js
-// @downloadURL  https://github.com/gil9red/user-scripts/raw/main/helpdesk.compassluxe/Кнопка копирования компонентов в буфер обмена.user.js
+// @homepage     https://github.com/gil9red/user-scripts/blob/main/helpdesk.compassluxe/Кнопка%20копирования%20компонентов%20в%20буфер%20обмена.user.js
+// @updateURL    https://github.com/gil9red/user-scripts/raw/main/helpdesk.compassluxe/Кнопка%20копирования%20компонентов%20в%20буфер%20обмена.user.js
+// @downloadURL  https://github.com/gil9red/user-scripts/raw/main/helpdesk.compassluxe/Кнопка%20копирования%20компонентов%20в%20буфер%20обмена.user.js
 // ==/UserScript==
 
 (function() {
     'use strict';
 
-    const id = "copying-components-to-clipboard";
+    const button_id = "copying-components-to-clipboard";
+    const components_field_selector = "#components-field";
 
     function processComponentField() {
-        const el = document.querySelector("#components-val");
+        const el = document.querySelector(components_field_selector);
         if (!el) return;
-        if (el.querySelector(`#${id}`)) return;
+        if (el.querySelector(`#${button_id}`)) return;
 
         el.insertAdjacentHTML(
             'beforeend',
             `
             <span
-                id="${id}"
+                id="${button_id}"
                 class="aui-button aui-style"
                 title="Копирование в буфер обмена"
                 style="padding: 4px; font-size: 10px;"
@@ -38,13 +39,18 @@
     }
 
     document.addEventListener('click', function(event) {
-        const button = event.target.closest(`#${id}`);
+        const button = event.target.closest(`#${button_id}`);
         if (!button) return;
 
         event.stopPropagation();
         event.preventDefault();
 
-        const text = document.querySelector("#components-field").textContent.split(",").map(obj => obj.trim()).join(", ");
+        const text = document
+            .querySelector(components_field_selector)
+            .textContent
+            .split(",")
+            .map(obj => obj.trim())
+            .join(", ");
         const info = button.querySelector(".info");
 
         info.textContent = " - копирование...";
