fd3de1b526545728384bf1fa1436484d0242c1e7
Ilya Petrash <ilya.petrash@inbox.ru>
2026-09-08 20:13:36 +0500

    Update Заполнение заголовка вкладки из текста поиска.user.js

--- rutracker/Заполнение заголовка вкладки из текста поиска.user.js
+++ rutracker/Заполнение заголовка вкладки из текста поиска.user.js
@@ -1,16 +1,16 @@
 // ==UserScript==
 // @name         Rutracker. Заполнение заголовка вкладки из текста поиска
 // @namespace    gil9red
-// @version      0.3
+// @version      2026-09-08
 // @description  try to take over the world!
 // @author       You
 // @match        https://rutracker.org/forum/tracker.php?*nm=*
 // @match        https://rutracker.net/forum/tracker.php?*nm=*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=rutracker.org
 // @grant        none
-// @homepage     https://github.com/gil9red/user-scripts
-// @updateURL    https://github.com/gil9red/user-scripts/raw/main/rutracker/Заполнение заголовка вкладки из текста поиска.user.js
-// @downloadURL  https://github.com/gil9red/user-scripts/raw/main/rutracker/Заполнение заголовка вкладки из текста поиска.user.js
+// @homepage     https://github.com/gil9red/user-scripts/blob/main/rutracker/Заполнение%20заголовка%20вкладки%20из%20текста%20поиска.user.js
+// @updateURL    https://github.com/gil9red/user-scripts/raw/main/rutracker/Заполнение%20заголовка%20вкладки%20из%20текста%20поиска.user.js
+// @downloadURL  https://github.com/gil9red/user-scripts/raw/main/rutracker/Заполнение%20заголовка%20вкладки%20из%20текста%20поиска.user.js
 // ==/UserScript==
 
 (function() {
@@ -18,7 +18,7 @@
 
     let searchEl = document.getElementById("title-search");
     if (!searchEl) {
-        alert("Не удалось найти поле поиска!");
+        console.error("Не удалось найти поле поиска!");
         return;
     }
 
